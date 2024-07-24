666af477384d2e1019648f0a2edd3aab510137f7
Maheshwar  Nag <116495825+maheshwar04@users.noreply.github.com>
2024-07-24 23:29:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/CALCULATOR-1/"
+  base: "/CALCULATOR-1/",
+  plugins: [react()]
 })
