774015039d88497ac7f765cbb7bec13f1383addb
maheshwar04 <maheshwarnag04@gmail.com>
2024-07-24 23:31:19 +0530

    n

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CALCULATOR-1/"
+  base: "/CALCULATOR-1/",
+  resolve:{
+    alias:{
+      '@':Path.resolve(__dirname,'./src'),
+    },
+  },
 })
